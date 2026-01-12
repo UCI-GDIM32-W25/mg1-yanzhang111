1388828c7838b2aa4503aa78f53c85d000a86d19
yanzhang111 <yanz76@uci.edu>
2026-01-11 20:39:30 -0800

    1

--- Assets/Scripts/PlantCountUI.cs
+++ Assets/Scripts/PlantCountUI.cs
@@ -6,8 +6,10 @@ public class PlantCountUI : MonoBehaviour
     [SerializeField] private TMP_Text _plantedText;
     [SerializeField] private TMP_Text _remainingText;
 
-    public void UpdateSeeds (int seedsLeft, int seedsPlanted)
+    public void UpdateSeeds(int seedsLeft, int seedsPlanted)
     {
-        
+        _remainingText.text = "Seeds Left: " + seedsLeft;
+        _plantedText.text = "Seeds Planted: " + seedsPlanted;
     }
 }
+
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,24 +5,45 @@ public class Player : MonoBehaviour
     [SerializeField] private float _speed = 1.0f;
     [SerializeField] private Transform _playerTransform;
     [SerializeField] private GameObject _plantPrefab;
-    [SerializeField] private int _numSeeds = 5; 
+    [SerializeField] private int _numSeeds = 5;
     [SerializeField] private PlantCountUI _plantCountUI;
 
     private int _numSeedsLeft;
     private int _numSeedsPlanted;
 
-    private void Start ()
+    private void Start()
     {
-        
+        _numSeedsLeft = _numSeeds;
+        _numSeedsPlanted = 0;
+        _plantCountUI.UpdateSeeds(_numSeedsLeft, _numSeedsPlanted);
     }
 
     private void Update()
     {
-        
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+
+        Vector3 movement = new Vector3(moveX, moveY, 0f);
+        _playerTransform.Translate(movement * _speed * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            PlantSeed();
+        }
     }
 
-    public void PlantSeed ()
+    public void PlantSeed()
     {
-        
+ 
+        if (_numSeedsLeft <= 0)
+        {
+            return;
+        }
+        Instantiate(_plantPrefab, _playerTransform.position, Quaternion.identity);
+
+        _numSeedsLeft--;
+        _numSeedsPlanted++;
+
+        _plantCountUI.UpdateSeeds(_numSeedsLeft, _numSeedsPlanted);
     }
 }
